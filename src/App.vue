59f99b91f53f7a9947d6c8b32d45ba28d3b6c562
lei.shiyun <lei.shiyun@astute-tec.com>
2022-01-11 23:38:09 +0800

    项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header></Header>
+    <!-- 路由组件的出口 -->
+    <router-view></router-view>
+    <Footer v-show="$route.meta.show"></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from "./components/footer/index.vue";
+import Header from "./components/header/index.vue";
+
+export default {
+    name: "App",
+    components: { Footer, Header }
+}
+</script>
+
+<style>
+</style>
